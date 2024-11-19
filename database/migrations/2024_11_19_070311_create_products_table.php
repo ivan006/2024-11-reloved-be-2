6a3974dbd96ee12363e482571d766e7fc841cb54
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-19 09:16:51 +0200

    1

--- database/migrations/2024_11_19_070311_create_products_table.php
+++ database/migrations/2024_11_19_070311_create_products_table.php
@@ -18,17 +18,24 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->decimal('price', 8, 2);
             $table->string('image')->nullable(); // Nullable image attribute
+            $table->date('purchase_date'); // Date the product was purchased
             $table->timestamps();
 
             // Constraints
             $table->unsignedInteger('brand_id');
             $table->unsignedInteger('gender_id');
             $table->unsignedInteger('category_id');
+            $table->unsignedInteger('seller_id'); // Links to users table for the seller
+            $table->unsignedInteger('buyer_id'); // Links to users table for the buyer
 
+            // Foreign key constraints
             $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
             $table->foreign('gender_id')->references('id')->on('genders')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('seller_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('buyer_id')->references('id')->on('users')->onDelete('cascade');
         });
+
 
     }
 
