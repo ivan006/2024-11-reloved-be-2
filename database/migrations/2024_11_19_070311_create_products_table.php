bb90470644b7c70b473b82535697592801eb3aca
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-19 09:03:36 +0200

    1

--- database/migrations/2024_11_19_070311_create_products_table.php
+++ database/migrations/2024_11_19_070311_create_products_table.php
@@ -9,21 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique(); // Category names like "T-Shirts", "Pants", etc.
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 };
