d20e5fc28d1126b716f59fb23c8da80fd6b12c78
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-19 09:01:52 +0200

    1

--- database/migrations/2024_11_19_070120_create_genders_table.php
+++ database/migrations/2024_11_19_070120_create_genders_table.php
@@ -9,21 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('genders', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique(); // Each brand name must be unique
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('genders');
     }
 };
