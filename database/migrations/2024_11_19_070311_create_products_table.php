35add56fe7a8939a105f9a493a1e2ef72175d3f5
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-19 09:03:54 +0200

    1

--- database/migrations/2024_11_19_070311_create_products_table.php
+++ database/migrations/2024_11_19_070311_create_products_table.php
@@ -9,13 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->string('image')->nullable(); // Nullable image attribute
+            $table->foreignId('brand_id')->constrained()->cascadeOnDelete(); // Links to brands table
+            $table->foreignId('gender_id')->constrained()->cascadeOnDelete(); // Links to genders table
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete(); // Links to categories table
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
